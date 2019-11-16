9d6c8d39411274b1a81834811e7a7546a26f6ed9
Jarvis Luong <jarvisluong@gmail.com>
2019-11-16 17:27:50 +0200

    add source_code route

---
+++ pages/source_code.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SourceCode = () => {
+  return (
+    <>
+      <a href="https://github.com/jarvisluong/macdream-frontend">
+        Front-end code
+      </a>
+      <a href="https://github.com/jarvisluong/macdream-backend">
+        Back-end code
+      </a>
+    </>
+  );
+};
+
+export default SourceCode;
