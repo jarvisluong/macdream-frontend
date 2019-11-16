02a6d01ebea72c5d6ea14fddcc360d53093f48a6
Janet Van <janet@bankify.com.com>
2019-11-16 19:56:57 +0200

    request to create a transaction

--- macdream.dtos.ts
+++ macdream.dtos.ts
@@ -14,322 +14,376 @@ BaseUrl: http://167.172.165.116
 //DefaultImports: 
 */
 
-
-export interface IReturn<T>
-{
-    createResponse(): T;
+export interface IReturn<T> {
+  createResponse(): T;
 }
 
-export interface IReturnVoid
-{
-    createResponse(): void;
+export interface IReturnVoid {
+  createResponse(): void;
 }
 
-export class TransactionDto
-{
-    public paymentDt: string;
-    public description: string;
+export class TransactionDto {
+  public paymentDt: string;
+  public description: string;
 
-    public constructor(init?: Partial<TransactionDto>) { (Object as any).assign(this, init); }
+  public constructor(init?: Partial<TransactionDto>) {
+    (Object as any).assign(this, init);
+  }
 }
 
-export class PersonJson
-{
-    public id: number;
-    public name: string;
-    public transactions: TransactionDto[];
+export class PersonJson {
+  public id: number;
+  public name: string;
+  public transactions: TransactionDto[];
 
-    public constructor(init?: Partial<PersonJson>) { (Object as any).assign(this, init); }
+  public constructor(init?: Partial<PersonJson>) {
+    (Object as any).assign(this, init);
+  }
 }
 
-export enum VisaMccEnum
-{
-    Missing = 0,
-    Electronics = 1234,
-    Coffee = 1235,
-    Rent = 1236,
-    Food = 1237,
-    Alcohol = 1238,
-    Deposit = 7777,
-    Withdrawal = 7778,
+export enum VisaMccEnum {
+  Missing = 0,
+  Electronics = 1234,
+  Coffee = 1235,
+  Rent = 1236,
+  Food = 1237,
+  Alcohol = 1238,
+  Deposit = 7777,
+  Withdrawal = 7778
 }
 
-export class QueryBase
-{
-    // @DataMember(Order=1)
-    public skip: number;
+export class QueryBase {
+  // @DataMember(Order=1)
+  public skip: number;
 
-    // @DataMember(Order=2)
-    public take: number;
+  // @DataMember(Order=2)
+  public take: number;
 
-    // @DataMember(Order=3)
-    public orderBy: string;
+  // @DataMember(Order=3)
+  public orderBy: string;
 
-    // @DataMember(Order=4)
-    public orderByDesc: string;
+  // @DataMember(Order=4)
+  public orderByDesc: string;
 
-    // @DataMember(Order=5)
-    public include: string;
+  // @DataMember(Order=5)
+  public include: string;
 
-    // @DataMember(Order=6)
-    public fields: string;
+  // @DataMember(Order=6)
+  public fields: string;
 
-    // @DataMember(Order=7)
-    public meta: { [index: string]: string; };
+  // @DataMember(Order=7)
+  public meta: { [index: string]: string };
 
-    public constructor(init?: Partial<QueryBase>) { (Object as any).assign(this, init); }
+  public constructor(init?: Partial<QueryBase>) {
+    (Object as any).assign(this, init);
+  }
 }
 
-export class QueryDb<T> extends QueryBase
-{
-
-    public constructor(init?: Partial<QueryDb<T>>) { super(init); (Object as any).assign(this, init); }
+export class QueryDb<T> extends QueryBase {
+  public constructor(init?: Partial<QueryDb<T>>) {
+    super(init);
+    (Object as any).assign(this, init);
+  }
 }
 
-export enum GoalTypeEnum
-{
-    Purchase = 'Purchase',
-    CashSaving = 'CashSaving',
-    Investing = 'Investing',
+export enum GoalTypeEnum {
+  Purchase = "Purchase",
+  CashSaving = "CashSaving",
+  Investing = "Investing"
 }
 
-export class GoalTbl
-{
-    public id: number;
-    // @References(Type=typeof(PersonTbl))
-    public personId: number;
+export class GoalTbl {
+  public id: number;
+  // @References(Type=typeof(PersonTbl))
+  public personId: number;
 
-    // @Required()
-    public targetDt: string;
+  // @Required()
+  public targetDt: string;
 
-    // @Required()
-    public goalType: GoalTypeEnum;
+  // @Required()
+  public goalType: GoalTypeEnum;
 
-    public saving: number;
-    public price: number;
-    // @StringLength(MaximumLength=100, MinimumLength=1)
-    public name: string;
+  public saving: number;
+  public price: number;
+  // @StringLength(MaximumLength=100, MinimumLength=1)
+  public name: string;
 
-    // @StringLength(MaximumLength=255, MinimumLength=1)
-    public description: string;
+  // @StringLength(MaximumLength=255, MinimumLength=1)
+  public description: string;
 
-    public constructor(init?: Partial<GoalTbl>) { (Object as any).assign(this, init); }
-}
-
-// @DataContract
-export class ResponseError
-{
-    // @DataMember(Order=1, EmitDefaultValue=false)
-    public errorCode: string;
-
-    // @DataMember(Order=2, EmitDefaultValue=false)
-    public fieldName: string;
-
-    // @DataMember(Order=3, EmitDefaultValue=false)
-    public message: string;
-
-    // @DataMember(Order=4, EmitDefaultValue=false)
-    public meta: { [index: string]: string; };
-
-    public constructor(init?: Partial<ResponseError>) { (Object as any).assign(this, init); }
+  public constructor(init?: Partial<GoalTbl>) {
+    (Object as any).assign(this, init);
+  }
 }
 
 // @DataContract
-export class ResponseStatus
-{
-    // @DataMember(Order=1)
-    public errorCode: string;
+export class ResponseError {
+  // @DataMember(Order=1, EmitDefaultValue=false)
+  public errorCode: string;
 
-    // @DataMember(Order=2)
-    public message: string;
+  // @DataMember(Order=2, EmitDefaultValue=false)
+  public fieldName: string;
 
-    // @DataMember(Order=3)
-    public stackTrace: string;
+  // @DataMember(Order=3, EmitDefaultValue=false)
+  public message: string;
 
-    // @DataMember(Order=4)
-    public errors: ResponseError[];
+  // @DataMember(Order=4, EmitDefaultValue=false)
+  public meta: { [index: string]: string };
 
-    // @DataMember(Order=5)
-    public meta: { [index: string]: string; };
-
-    public constructor(init?: Partial<ResponseStatus>) { (Object as any).assign(this, init); }
-}
-
-export class PersonTbl
-{
-    public id: number;
-    // @StringLength(MaximumLength=255, MinimumLength=1)
-    public name: string;
-
-    public balance: number;
-
-    public constructor(init?: Partial<PersonTbl>) { (Object as any).assign(this, init); }
-}
-
-export class TransactionTbl
-{
-    public id: number;
-    // @References(Type=typeof(PersonTbl))
-    public personId: number;
-
-    // @References(Type=typeof(VisaMccTbl))
-    public visaMccId: number;
-
-    // @Required()
-    public paymentDt: string;
-
-    public price: number;
-    // @StringLength(MaximumLength=255, MinimumLength=1)
-    public description: string;
-
-    public constructor(init?: Partial<TransactionTbl>) { (Object as any).assign(this, init); }
-}
-
-export class VisaMccTbl
-{
-    public id: number;
-    // @Required()
-    public visaMcc: VisaMccEnum;
-
-    public isSaving: boolean;
-
-    public constructor(init?: Partial<VisaMccTbl>) { (Object as any).assign(this, init); }
-}
-
-export class GetAllThePeopleAndTransactionsResponse
-{
-    public people: PersonJson[];
-
-    public constructor(init?: Partial<GetAllThePeopleAndTransactionsResponse>) { (Object as any).assign(this, init); }
-}
-
-export class UpdateVisaMccResponse
-{
-    public visaMccId: number;
-    public visaMcc: VisaMccEnum;
-    public isSaving: boolean;
-
-    public constructor(init?: Partial<UpdateVisaMccResponse>) { (Object as any).assign(this, init); }
-}
-
-export class UpdateGoalResponse
-{
-    public goalId: number;
-    public goalSaving: number;
-    public goalPrice: number;
-    public goalAchieved: boolean;
-
-    public constructor(init?: Partial<UpdateGoalResponse>) { (Object as any).assign(this, init); }
-}
-
-export class InsertNewTransactionResponse
-{
-    public newTransactionId: number;
-
-    public constructor(init?: Partial<InsertNewTransactionResponse>) { (Object as any).assign(this, init); }
+  public constructor(init?: Partial<ResponseError>) {
+    (Object as any).assign(this, init);
+  }
 }
 
 // @DataContract
-export class QueryResponse<T>
-{
-    // @DataMember(Order=1)
-    public offset: number;
+export class ResponseStatus {
+  // @DataMember(Order=1)
+  public errorCode: string;
 
-    // @DataMember(Order=2)
-    public total: number;
+  // @DataMember(Order=2)
+  public message: string;
 
-    // @DataMember(Order=3)
-    public results: T[];
+  // @DataMember(Order=3)
+  public stackTrace: string;
 
-    // @DataMember(Order=4)
-    public meta: { [index: string]: string; };
+  // @DataMember(Order=4)
+  public errors: ResponseError[];
 
-    // @DataMember(Order=5)
-    public responseStatus: ResponseStatus;
+  // @DataMember(Order=5)
+  public meta: { [index: string]: string };
 
-    public constructor(init?: Partial<QueryResponse<T>>) { (Object as any).assign(this, init); }
+  public constructor(init?: Partial<ResponseStatus>) {
+    (Object as any).assign(this, init);
+  }
+}
+
+export class PersonTbl {
+  public id: number;
+  // @StringLength(MaximumLength=255, MinimumLength=1)
+  public name: string;
+
+  public balance: number;
+
+  public constructor(init?: Partial<PersonTbl>) {
+    (Object as any).assign(this, init);
+  }
+}
+
+export class TransactionTbl {
+  public id: number;
+  // @References(Type=typeof(PersonTbl))
+  public personId: number;
+
+  // @References(Type=typeof(VisaMccTbl))
+  public visaMccId: number;
+
+  // @Required()
+  public paymentDt: string;
+
+  public price: number;
+  // @StringLength(MaximumLength=255, MinimumLength=1)
+  public description: string;
+
+  public constructor(init?: Partial<TransactionTbl>) {
+    (Object as any).assign(this, init);
+  }
+}
+
+export class VisaMccTbl {
+  public id: number;
+  // @Required()
+  public visaMcc: VisaMccEnum;
+
+  public isSaving: boolean;
+
+  public constructor(init?: Partial<VisaMccTbl>) {
+    (Object as any).assign(this, init);
+  }
+}
+
+export class GetAllThePeopleAndTransactionsResponse {
+  public people: PersonJson[];
+
+  public constructor(init?: Partial<GetAllThePeopleAndTransactionsResponse>) {
+    (Object as any).assign(this, init);
+  }
+}
+
+export class UpdateVisaMccResponse {
+  public visaMccId: number;
+  public visaMcc: VisaMccEnum;
+  public isSaving: boolean;
+
+  public constructor(init?: Partial<UpdateVisaMccResponse>) {
+    (Object as any).assign(this, init);
+  }
+}
+
+export class UpdateGoalResponse {
+  public goalId: number;
+  public goalSaving: number;
+  public goalPrice: number;
+  public goalAchieved: boolean;
+
+  public constructor(init?: Partial<UpdateGoalResponse>) {
+    (Object as any).assign(this, init);
+  }
+}
+
+export class InsertNewTransactionResponse {
+  public newTransactionId: number;
+
+  public constructor(init?: Partial<InsertNewTransactionResponse>) {
+    (Object as any).assign(this, init);
+  }
+}
+
+// @DataContract
+export class QueryResponse<T> {
+  // @DataMember(Order=1)
+  public offset: number;
+
+  // @DataMember(Order=2)
+  public total: number;
+
+  // @DataMember(Order=3)
+  public results: T[];
+
+  // @DataMember(Order=4)
+  public meta: { [index: string]: string };
+
+  // @DataMember(Order=5)
+  public responseStatus: ResponseStatus;
+
+  public constructor(init?: Partial<QueryResponse<T>>) {
+    (Object as any).assign(this, init);
+  }
 }
 
 // @Route("/demo", "GET")
-export class GetAllThePeopleAndTransactionsRequest implements IReturn<GetAllThePeopleAndTransactionsResponse>
-{
-
-    public constructor(init?: Partial<GetAllThePeopleAndTransactionsRequest>) { (Object as any).assign(this, init); }
-    public createResponse() { return new GetAllThePeopleAndTransactionsResponse(); }
-    public getTypeName() { return 'GetAllThePeopleAndTransactionsRequest'; }
+export class GetAllThePeopleAndTransactionsRequest
+  implements IReturn<GetAllThePeopleAndTransactionsResponse> {
+  public constructor(init?: Partial<GetAllThePeopleAndTransactionsRequest>) {
+    (Object as any).assign(this, init);
+  }
+  public createResponse() {
+    return new GetAllThePeopleAndTransactionsResponse();
+  }
+  public getTypeName() {
+    return "GetAllThePeopleAndTransactionsRequest";
+  }
 }
 
 // @Route("/visamcc/update", "PUT")
-export class UpdateVisaMccRequest implements IReturn<UpdateVisaMccResponse>
-{
-    public visaMccId: number;
-    public isSaving: boolean;
+export class UpdateVisaMccRequest implements IReturn<UpdateVisaMccResponse> {
+  public visaMccId: number;
+  public isSaving: boolean;
 
-    public constructor(init?: Partial<UpdateVisaMccRequest>) { (Object as any).assign(this, init); }
-    public createResponse() { return new UpdateVisaMccResponse(); }
-    public getTypeName() { return 'UpdateVisaMccRequest'; }
+  public constructor(init?: Partial<UpdateVisaMccRequest>) {
+    (Object as any).assign(this, init);
+  }
+  public createResponse() {
+    return new UpdateVisaMccResponse();
+  }
+  public getTypeName() {
+    return "UpdateVisaMccRequest";
+  }
 }
 
 // @Route("/goals/update", "PUT")
-export class UpdateGoalRequest implements IReturn<UpdateGoalResponse>
-{
-    public personId: number;
-    public goalId: number;
-    public amount: number;
-    public description: string;
+export class UpdateGoalRequest implements IReturn<UpdateGoalResponse> {
+  public personId: number;
+  public goalId: number;
+  public amount: number;
+  public description: string;
 
-    public constructor(init?: Partial<UpdateGoalRequest>) { (Object as any).assign(this, init); }
-    public createResponse() { return new UpdateGoalResponse(); }
-    public getTypeName() { return 'UpdateGoalRequest'; }
+  public constructor(init?: Partial<UpdateGoalRequest>) {
+    (Object as any).assign(this, init);
+  }
+  public createResponse() {
+    return new UpdateGoalResponse();
+  }
+  public getTypeName() {
+    return "UpdateGoalRequest";
+  }
 }
 
 // @Route("/transactions/new", "POST")
-export class InsertNewTransactionRequest implements IReturn<InsertNewTransactionResponse>
-{
-    public personId: number;
-    public paymentDt: string;
-    public price: number;
-    public visaMccId: number;
-    public description: string;
+export class InsertNewTransactionRequest
+  implements IReturn<InsertNewTransactionResponse> {
+  public personId: number;
+  public paymentDt: string;
+  public price: number;
+  public visaMccId: number;
+  public description: string;
 
-    public constructor(init?: Partial<InsertNewTransactionRequest>) { (Object as any).assign(this, init); }
-    public createResponse() { return new InsertNewTransactionResponse(); }
-    public getTypeName() { return 'InsertNewTransactionRequest'; }
+  public constructor(init?: Partial<InsertNewTransactionRequest>) {
+    (Object as any).assign(this, init);
+  }
+  public createResponse() {
+    return new InsertNewTransactionResponse();
+  }
+  public getTypeName() {
+    return "InsertNewTransactionRequest";
+  }
 }
 
 // @Route("/autoquery/goals")
-export class QueryGoals extends QueryDb<GoalTbl> implements IReturn<QueryResponse<GoalTbl>>
-{
-
-    public constructor(init?: Partial<QueryGoals>) { super(init); (Object as any).assign(this, init); }
-    public createResponse() { return new QueryResponse<GoalTbl>(); }
-    public getTypeName() { return 'QueryGoals'; }
+export class QueryGoals extends QueryDb<GoalTbl>
+  implements IReturn<QueryResponse<GoalTbl>> {
+  public constructor(init?: Partial<QueryGoals>) {
+    super(init);
+    (Object as any).assign(this, init);
+  }
+  public createResponse() {
+    return new QueryResponse<GoalTbl>();
+  }
+  public getTypeName() {
+    return "QueryGoals";
+  }
 }
 
 // @Route("/autoquery/persons")
-export class QueryPersons extends QueryDb<PersonTbl> implements IReturn<QueryResponse<PersonTbl>>
-{
-
-    public constructor(init?: Partial<QueryPersons>) { super(init); (Object as any).assign(this, init); }
-    public createResponse() { return new QueryResponse<PersonTbl>(); }
-    public getTypeName() { return 'QueryPersons'; }
+export class QueryPersons extends QueryDb<PersonTbl>
+  implements IReturn<QueryResponse<PersonTbl>> {
+  public constructor(init?: Partial<QueryPersons>) {
+    super(init);
+    (Object as any).assign(this, init);
+  }
+  public createResponse() {
+    return new QueryResponse<PersonTbl>();
+  }
+  public getTypeName() {
+    return "QueryPersons";
+  }
 }
 
 // @Route("/autoquery/transactions")
-export class QueryTransactions extends QueryDb<TransactionTbl> implements IReturn<QueryResponse<TransactionTbl>>
-{
-
-    public constructor(init?: Partial<QueryTransactions>) { super(init); (Object as any).assign(this, init); }
-    public createResponse() { return new QueryResponse<TransactionTbl>(); }
-    public getTypeName() { return 'QueryTransactions'; }
+export class QueryTransactions extends QueryDb<TransactionTbl>
+  implements IReturn<QueryResponse<TransactionTbl>> {
+  public constructor(init?: Partial<QueryTransactions>) {
+    super(init);
+    (Object as any).assign(this, init);
+  }
+  public createResponse() {
+    return new QueryResponse<TransactionTbl>();
+  }
+  public getTypeName() {
+    return "QueryTransactions";
+  }
 }
 
 // @Route("/autoquery/visamccs")
-export class QueryVisaMccs extends QueryDb<VisaMccTbl> implements IReturn<QueryResponse<VisaMccTbl>>
-{
-
-    public constructor(init?: Partial<QueryVisaMccs>) { super(init); (Object as any).assign(this, init); }
-    public createResponse() { return new QueryResponse<VisaMccTbl>(); }
-    public getTypeName() { return 'QueryVisaMccs'; }
+export class QueryVisaMccs extends QueryDb<VisaMccTbl>
+  implements IReturn<QueryResponse<VisaMccTbl>> {
+  public constructor(init?: Partial<QueryVisaMccs>) {
+    super(init);
+    (Object as any).assign(this, init);
+  }
+  public createResponse() {
+    return new QueryResponse<VisaMccTbl>();
+  }
+  public getTypeName() {
+    return "QueryVisaMccs";
+  }
 }
-
--- pages/api/transactions.ts
+++ pages/api/transactions.ts
@@ -1,21 +1,48 @@
 import {
   QueryTransactions,
-//   InsertNewTransactionRequest
+  InsertNewTransactionRequest
 } from "../../macdream.dtos";
 import { client } from "../../lib/client";
-import { NextApiResponse, NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function queryTransactions(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const result = await client.get(new QueryTransactions());
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
+export default async function createTransaction(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+  // throw new Error("not implemented");
+  switch (method) {
+    case "POST":
+      try {
+        const body = {
+          personId: 1,
+          PaymentDt: "2019.11.09",
+          Price: 10.0,
+          VisaMccId: 1,
+          Description: "AAA"
+        };
+
+        const transRes = await client.post(
+          new InsertNewTransactionRequest(body)
+        );
+        console.log({ transRes });
+        res.status(200).json(transRes);
+      } catch (error) {
+        console.log({ error });
+        res.status(400).json(error.responseStatus.message);
+      }
+      break;
+
+    case "GET":
+      try {
+        const { responseStatus, results } = await client.get(
+          new QueryTransactions()
+        );
+        res.status(parseInt(responseStatus.errorCode) || 400).json(results);
+      } catch (error) {}
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
   }
-}
-
-// Not working atm
-export async function createTransaction() {
-  throw new Error("not implemented");
-  // return await client.post(new InsertNewTransactionRequest, body)
 }
