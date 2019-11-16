26c8a7fc45e6be716b698e2acd0b03b5056067ec
Jarvis Luong <jarvisluong@gmail.com>
2019-11-16 17:28:57 +0200

    update source code code

--- pages/source_code.js
+++ pages/source_code.js
@@ -3,12 +3,19 @@ import React from "react";
 const SourceCode = () => {
   return (
     <>
-      <a href="https://github.com/jarvisluong/macdream-frontend">
-        Front-end code
-      </a>
-      <a href="https://github.com/jarvisluong/macdream-backend">
-        Back-end code
-      </a>
+      <h1>MacDream Source Code</h1>
+      <ul>
+        <li>
+          <a href="https://github.com/jarvisluong/macdream-frontend">
+            Front-end code
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/jarvisluong/macdream-backend">
+            Back-end code
+          </a>
+        </li>
+      </ul>
     </>
   );
 };
