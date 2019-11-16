ec2adb46cc25ccb9e6325a70188696dd1620b5cc
Jarvis Luong <jarvisluong@gmail.com>
2019-11-16 20:42:24 +0200

    done

--- pages/api/transactions.ts
+++ pages/api/transactions.ts
@@ -5,6 +5,16 @@ import {
 import { client } from "../../lib/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const FoodDescriptions = ["Burger", "Pizza"];
+const AlcoholDescriptions = ["Beer", "Alko", "Wine"];
+const CoffeeDescriptiosn = ["Starbuck", "EspressoHouse"];
+
+const mccMap = {
+  1: AlcoholDescriptions,
+  2: CoffeeDescriptiosn,
+  5: FoodDescriptions
+};
+
 export default async function createTransaction(
   req: NextApiRequest,
   res: NextApiResponse
@@ -14,12 +24,20 @@ export default async function createTransaction(
   switch (method) {
     case "POST":
       try {
+        const visaMccids = [1, 2, 5];
+        const ramdomChosenVisaMccId =
+          visaMccids[Math.floor(Math.random() * visaMccids.length)];
+        const descriptions = mccMap[ramdomChosenVisaMccId];
+        const randomChosenDescription =
+        descriptions[
+            Math.floor(Math.random() * descriptions.length)
+          ];
         const body = {
           personId: 1,
           PaymentDt: "2019.11.09",
           Price: 10.0,
-          VisaMccId: 1,
-          Description: "AAA"
+          VisaMccId: ramdomChosenVisaMccId,
+          Description: randomChosenDescription
         };
 
         const transRes = await client.post(
@@ -35,11 +53,13 @@ export default async function createTransaction(
 
     case "GET":
       try {
-        const { responseStatus, results } = await client.get(
-          new QueryTransactions()
-        );
-        res.status(parseInt(responseStatus.errorCode) || 400).json(results);
-      } catch (error) {}
+        const body = new QueryTransactions();
+        body.orderBy = "-paymentDt";
+        const results = await client.get(body);
+        res.status(200).json(results);
+      } catch (error) {
+        res.status(400).json(error.message);
+      }
       break;
     default:
       res.setHeader("Allow", ["GET", "POST"]);
